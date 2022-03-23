9dd188784838df1733fcc0edbe4b990def6a788c
muhammadtaufikhidayat <muhtopik07@gmail.com>
2022-03-23 22:12:31 +0700

    Pisah class Taxi ke file yang berbeda

--- Program.cs
+++ Program.cs
@@ -1,25 +1,8 @@
 ﻿using System;
+using ClassTaxi;
 
 namespace Tugas
 {
-    public class Taxi
-    {
-        public string DriverName { get; set; }
-        public bool OnDuty { get; set; }
-        public int NumPassenger { get; set; }
-    
-        public void TaxiInfo(){
-            Console.WriteLine("Driver Name: {0}",DriverName);
-            Console.WriteLine("On Duty: {0}", OnDuty);
-            Console.WriteLine("Number of Passenger: {0}",NumPassenger);
-        }
-        public void PickUpPassenger(){
-            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
-        }
-        public void PickOffPassenger(){
-            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
